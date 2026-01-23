f52d6b5c8bd1ee1e3748638c80e84801090fcb54
Rayimbek <rayimbeknarziyev2@gmail.com>
2026-01-23 15:17:25 +0500

    xatolikni taxladim

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+// Sizning Supabase ma'lumotlaringiz
+const SUPABASE_URL = 'https://xhbokwqbxxqgyxfkljtl.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhoYm9rd3FieHhxZ3l4ZmtsanRsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjkxNDM3MjQsImV4cCI6MjA4NDcxOTcyNH0.GmRD3KAktubwU4zXPexqZCE2UXsu7TFVpMyIjJruMVk';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
